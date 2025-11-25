d422f0d2db8487156c2963ca4b5041dda1ccfd2a
Deshan16 <136686925+Deshan16@users.noreply.github.com>
2025-11-25 20:40:11 +0530

    Add files via upload

---
+++ dist/server.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
+app.get("/", (_request, response) => {
+    response.send("<h1>Backend is running</h1>");
+});
+app.get("/page/:name", (request, response) => {
+    const file = request.params.name;
+    const filePath = path_1.default.join(__dirname, "html", file);
+    response.sendFile(filePath, (error) => {
+        if (error)
+            response.status(404).send("<h1>Page not found</h1>");
+    });
+});
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
